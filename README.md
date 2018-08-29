2172950ed475c5cfb29dcc3c0ff820824a62e280
Denys Odaruschenko <deod@ciklum.com>
2018-08-29 18:33:59 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Redux-flow
+
+https://www.youtube.com/watch?v=BS7fRPWmNZo&list=PLIcAMDxr6tprSzqKmfhDiW00GWbDcs8lE
---
+++ source/index.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+const playlist = (state = [], action) => {
+    console.log('action', action);
+
+    if (action.type === 'ADD_TRACK') {
+        return [
+            ...state,
+            action.payload,
+        ];
+    }
+}
+
+const store = createStore(playlist);
+
+store.subscribe(() => {
+    console.log('subscribe', store.getState());
+    const ul = document.getElementsByClassName('list')[0];
+    ul.innerHTML = '';
+    document.querySelectorAll('.trackInput')[0].value = '';
+    store.getState().forEach(track => {
+        const li = document.createElement('li');
+        li.innerHTML = track;
+        ul.appendChild(li);
+    })
+});
+
+
+export default class App extends Component {
+
+    componentDidMount() {
+        const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+        console.log(addTrackBtn);
+        addTrackBtn.addEventListener('click', () => {
+            const trackName = document.querySelectorAll('.trackInput')[0].value;
+            store.dispatch({type: 'ADD_TRACK', payload: trackName});
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <input type="text" className="trackInput"/>
+                <button className="addTrack">Add track</button>
+                <ul className="list"></ul>
+            </>
+        )
+    }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
